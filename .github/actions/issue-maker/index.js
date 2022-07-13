4e72cdb54adc8121e46b1ea928e1bb133d0a38d6
Wessley Alexander <wessleybug@gmail.com>
2022-07-13 16:27:31 -0500

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -17,14 +17,8 @@ async function run() {
     });
     
   await core.summary
-    .addHeading('Test Results')
-    .addTable([
-      [{data: 'File', header: true}, {data: 'Result', header: true}],
-      ['foo.js', 'Pass '],
-      ['bar.js', 'Fail '],
-      ['test.js', 'Pass ']
-    ])
-    .addLink('View staging deployment!', 'https://github.com')
+    .addHeading('READMEGEN')
+    .addRAW("\n\n*\n\nTest")
     .write()
   } catch (err) {
       core.setFailed(err.message);
